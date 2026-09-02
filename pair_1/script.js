4de3f0e284b28b8c1ae4a981764ef5b9b9d6a4f3
Mariia <mariiaikonnikova6@gmail.com>
2026-09-02 14:12:29 +0300

    Add files via upload

---
+++ pair_1/script.js
@@ -0,0 +1,24 @@
+// alert("Привіт");
+// const name = "Іван";
+//
+// let age = 0;
+//
+// const isStudent = true;
+//
+// console.log(typeof isStudent);
+
+let cash = prompt("Enter a cash");
+let productName = prompt("Enter your product name");
+let productPrice = +prompt("Enter your product price");
+let productCount = +prompt("Enter your product count");
+let delivery = +prompt("Enter your delivery price");
+let salePercent = +prompt("Enter your sale (%)");
+
+let fullPrice = (productPrice * productCount) + delivery;
+let discountAmount = (fullPrice * salePercent) / 100;
+let totalPrice = fullPrice - discountAmount;
+
+console.log(`Product name: ${productName}, Total price without sale: ${fullPrice}`);
+console.log(`Discount amount: ${discountAmount}, Final price: ${totalPrice}`);
+
+alert("Product: " + productName + "\nTotal price to pay is: " + totalPrice);
